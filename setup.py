4cd593d92230b93dbfe5fbbb38138827ac03786b
beneisner <beisner@princeton.edu>
2017-07-05 16:37:30 +0800

    added an arclimb package, a setup, and a makefile

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='arclimb',
+    version='0.1dev',
+    packages=['arclimb', ],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+)
